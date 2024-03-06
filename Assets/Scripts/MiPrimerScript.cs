9c080fd956b7015784b1e672e0b75dff57459528
meicrowave <meix.bbaa@gmail.com>
2024-03-06 14:25:36 +0100

    Mensaje Hola Mundo en la consola de comandos unity

--- Assets/Scripts/MiPrimerScript.cs
+++ Assets/Scripts/MiPrimerScript.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class MiPrimerScript : MonoBehaviour
 {
+    int edad;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Hola mundo!");   
     }
 
     // Update is called once per frame
